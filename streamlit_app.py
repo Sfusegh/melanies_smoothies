ce459b48bcfd40ed14d07b22c369a7d7c0893d1a
Sfusegh <163872458+Sfusegh@users.noreply.github.com>
2024-03-21 14:51:22 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -25,7 +25,7 @@ st.write('The name on your smoothie will be:', name_on_order)
 
 # session = get_active_session() #this is an extra session and no need, cause error which Baily helped me to comments out
 
-my_df=session.table("smoothies.public.fruit_options").select(col("Search_on"))
+my_df=session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'),col("Search_on"))
 # st.dataframe(data=my_df, use_container_width=True)
 # st.stop()
 pd_df=my_df.to_pandas()
@@ -45,9 +45,10 @@ if ingredients_list:
     ingredients_string=''
     for each_fruit_chosen in ingredients_list:
             ingredients_string+=each_fruit_chosen + ' '    
-            search_on=pd_df.loc[pd_df['FRUIT_NAME'] == fruit_chosen, 'SEARCH_ON'].iloc[0]
         
-            st.write('The search value for ', fruit_chosen,' is ', search_on, '.')
+            search_on=pd_df.loc[pd_df['FRUIT_NAME'] == each_fruit_chosen, 'SEARCH_ON'].iloc[0]        
+            st.write('The search value for ', each_fruit_chosen,' is ', search_on, '.')
+        
             st.subheader(each_fruit_chosen + "-Nutrition Fact")
         
             fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + each_fruit_chosen )
